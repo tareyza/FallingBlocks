71addb6def3a57d8d940727ce1a4ba6675be55ea
tareyza <tareyza@yahoo.com>
2015-07-29 10:56:19 -0400

    main activity gui made

--- app/src/main/java/com/jjapps/fallingblocks/MainActivity.java
+++ app/src/main/java/com/jjapps/fallingblocks/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
