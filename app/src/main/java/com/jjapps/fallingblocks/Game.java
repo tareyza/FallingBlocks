95b9ece3613342cffcc04f334e71579269153d8f
tareyza <tareyza@yahoo.com>
2015-07-29 11:32:22 -0400

    pause fragment added, added Play.onclick

--- app/src/main/java/com/jjapps/fallingblocks/Game.java
+++ app/src/main/java/com/jjapps/fallingblocks/Game.java
@@ -1,38 +1,23 @@
 package com.jjapps.fallingblocks;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Game extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
     }
-
-    public void launchGameActivity(View view){
-        Intent intent = new Intent(this, Game.class);
-        startActivity(intent);
-
-    }
-
-
-
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_game, menu);
         return true;
     }
 
